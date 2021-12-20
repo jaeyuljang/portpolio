25bad138371f216e61107062e462b21c8737c893
jaeyul <jaeyul89@naver.com>
2021-12-20 09:37:08 +0900

    다시 추가

--- js/main.js
+++ js/main.js
@@ -4,7 +4,7 @@ const typed = new Typed(".typing .txt", {
     "i am little <strong>slow</strong>",
     "but,try <strong>steadily</strong>",
     "I want to be a <strong>full stack developer</strong>.",
-    "my name is <strong>Jang sung ho</strong>",
+    "my name is <strong>Jang jae yul</strong>",
     "keep an <strong>eyes on me.</strong>",
   ],
   typeSpeed: 50,
